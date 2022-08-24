fe7aadf02d34edd999f131342ad2d417bf391eb8
MartinHernandez <martinhnz02@gmail.com>
2022-08-24 12:26:26 -0300

    Ejercicio2Prepractica

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -4,4 +4,27 @@
 
 #INICIO
 
+lista = []
+lista_impar = []
+x = 0
+
+for x in range(5):
+    numeros = int(input("Ingrese los valores de la lista: "))
+    if numeros % 2 :
+        lista_impar.append(numeros)
+    else: 
+        lista.append(numeros)
+    
+print("Lista completa: ") 
+lista_completa = lista + lista_impar
+print(lista_completa)
+
+print("Lista par")
+print(lista)
+
+print("Lista impar")
+print(lista_impar)
+
+
+ 
 #FIN
